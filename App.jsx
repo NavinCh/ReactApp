d03368b03e12003539ab9dec972f72b013ce836e
DESKTOP-7I4RASB\Naveen <ui.nrch@gmail.com>
2017-03-14 14:27:07 -0400

    Component lifecycle
    Lifecycle Methods
    componentWillMount is executed before rendering, on both server and
    client side.
    
    componentDidMount is executed after first render only on the client
    side. This is where AJAX requests and DOM or state updates should occur.
    This method is also used for integration with other JavaScript
    frameworks and any functions with delayed execution like setTimeout or
    setInterval. We are using it to update the state so we can trigger the
    other lifecycle methods.
    
    componentWillReceiveProps is invoked as soon as the props are updated
    before another render is called. We triggered it from setNewNumber when
    we updated the state.
    
    shouldComponentUpdate should return true or false value. This will
    determine if component will be updated or not. This is set to true by
    default. If you are sure that component doesn't need to render after
    state or props are updated, you can return false value.
    
    componentWillUpdate is called just before rendering.
    
    componentDidUpdate is called just after rendering.
    
    componentWillUnmount is called after the component is unmounted from the
    dom. We are unmounting our component in main.js.
    
    In our example we are setting initial state in constructor function. The
    setNewnumber is used to update the state. All the lifecycle methods are
    inside Content component.

--- App.jsx
+++ App.jsx
@@ -1,23 +1,62 @@
 import React from 'react';
-import ReactDom from 'react-dom';
 
 class App extends React.Component{
-	constructor(){
-		super();
+	constructor(props){
+		super(props);
+		this.state={
+			data: 0
+		}
 
-		this.findDomNodeHandler = this.findDomNodeHandler.bind(this);
+		this.setNewNumber = this.setNewNumber.bind(this);
 	};
 
-	findDomNodeHandler(){
-		var myDiv = document.getElementById('myDiv');
-		ReactDom.findDOMNode(myDiv).style.color = 'green';
-	};
+	setNewNumber(){
+		this.setState({data: this.state.data + 1})
+	}
 
 	render() {
 		return (
 			<div>
-				<button onClick={this.findDomNodeHandler}>find dom node</button>
-				<div id="myDiv">This div turns green on clicking button</div>
+				<button onClick={this.setNewNumber}>Increment</button>
+				<Content myNumber={this.state.data}></Content>
+			</div>
+		);
+	}
+}
+
+class Content extends React.Component{
+	componentWillMount(){
+		console.log('Component will Mount');
+	}
+
+	componentDidMount(){
+		console.log('Component did Mount');
+	}
+
+	componentWillReceiveProps(newProps){
+		console.log('Component will receive Props');
+	}
+
+	shouldComponentUpdate(newProps, newState){
+		return true;
+	}
+
+	componentWillUpdate(nextProps, nextState){
+		console.log('Component will update');
+	}
+
+	componentDidUpdate(prevProps, prevState){
+		console.log('Component did update');
+	}
+
+	componentWillUnmount(){
+		console.log('Component will unmount');
+	}
+
+	render() {
+		return (
+			<div>
+				<h3>{this.props.myNumber}</h3>
 			</div>
 		);
 	}
