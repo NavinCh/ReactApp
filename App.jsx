39f2ee5858eced13ad5a3c0b96f6f656af9ae5b6
DESKTOP-7I4RASB\Naveen <ui.nrch@gmail.com>
2017-03-14 12:55:11 -0400

    Force Update
    Force Update using forceUpdate() method

--- App.jsx
+++ App.jsx
@@ -4,25 +4,18 @@ class App extends React.Component{
 	constructor(){
 		super();
 
-		this.state = {
-			data: []
-		}
-
-		this.setStateHandler = this.setStateHandler.bind(this);
+		this.forceUpdateHandler = this.forceUpdateHandler.bind(this);
 	};
 
-	setStateHandler(){
-		var item = "set state....";
-		var myArray = this.state.data;
-		myArray.push(item);
-		this.setState({data:myArray})
+	forceUpdateHandler(){
+		this.forceUpdate();
 	};
 
 	render() {
 		return (
 			<div>
-				<button onClick={this.setStateHandler}>Set State</button>
-				<h4>State Array: {this.state.data}</h4>
+				<button onClick={this.forceUpdateHandler}>Force Update</button>
+				<h4>Random Number: {Math.random()}</h4>
 			</div>
 		);
 	}
