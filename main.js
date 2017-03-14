6ec25eca650d89849c97032b474957d5b96c381b
DESKTOP-7I4RASB\Naveen <ui.nrch@gmail.com>
2017-03-14 15:46:13 -0400

    React Forms
    In this example, used forms from child component. onChange method will
    trigger state update that will be passed to the child input value and
    rendered on screen. Whenever you need to update state from child
    component you need to pass function that will handle updating
    (updateState) as a prop (updateStateProp).

--- main.js
+++ main.js
@@ -3,7 +3,3 @@ import ReactDom from 'react-dom';
 import App from './App.jsx';
 
 ReactDom.render(<App headerProp="Header from props......." contentProp="Content from props....."/>, document.getElementById('app'));
-
-setTimeout(() => {
-	ReactDom.unmountComponentAtNode(document.getElementById('app'));	
-},10000);
